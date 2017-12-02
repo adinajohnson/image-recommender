fe05f98e9f3cd2c1bc8f1946ecac3c22efaadf9c
schumanns <lschuman@oberlin.edu>
2017-12-02 12:20:49 -0500

    Converted bytes thingy to dict in analyzeUrl.py

--- analyzeUrl.py
+++ analyzeUrl.py
@@ -20,15 +20,11 @@ params = urllib.parse.urlencode({
 
 body = "{'url':'http://sheepmountain.com/wp-content/uploads/2016/01/20150131-DSC_0640-3-1500x700.jpg'}"
 
-try:
-    # Execute the REST API call and get the response.
-    conn = http.client.HTTPSConnection('westcentralus.api.cognitive.microsoft.com')
-    conn.request("POST", "/vision/v1.0/analyze?%s" % params, body, headers)
-    response = conn.getresponse()
-    data = response.read()
-    tags = data['description']['tags']
-    print(tags)
-
-except Exception as e:
-    print('Error:')
-    print(e)
+# Execute the REST API call and get the response.
+conn = http.client.HTTPSConnection('westcentralus.api.cognitive.microsoft.com')
+conn.request("POST", "/vision/v1.0/analyze?%s" % params, body, headers)
+response = conn.getresponse()
+data_bytes = response.read()
+data = json.loads(data_bytes.decode())
+tags = data['description']['tags']
+print(tags)
