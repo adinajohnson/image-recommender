bd2396d49bc7532cb648a047270b316b651de5bb
Adina Johnson <adinacjohnson@gmail.com>
2017-12-02 12:10:48 -0500

    clean up analyzeUrl code

--- analyzeUrl.py
+++ analyzeUrl.py
@@ -1,43 +1,27 @@
 import http.client, urllib.request, urllib.parse, urllib.error, base64, json, os, requests
 
+# get env variable API key
 azure_key = os.environ.get('AZURE_KEY', None)
-
-
-###############################################
-#### Update or verify the following values. ###
-###############################################
-
-# Replace the subscription_key string value with your valid subscription key.
 subscription_key = azure_key
 
-# Replace or verify the region.
-#
-# You must use the same region in your REST API call as you used to obtain your subscription keys.
-# For example, if you obtained your subscription keys from the westus region, replace 
-# "westcentralus" in the URI below with "westus".
-#
-# NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
-# a free trial subscription key, you should not need to change this region.
 uri_base = 'westcentralus.api.cognitive.microsoft.com'
 
 headers = {
-    # Request headers.
+    # Request headers
     'Content-Type': 'application/json',
     'Ocp-Apim-Subscription-Key': subscription_key,
 }
 
 params = urllib.parse.urlencode({
-    # Request parameters. All of them are optional.
+    # Request parameters
     'visualFeatures': 'Description',
     'language': 'en',
 })
 
-# Replace the three dots below with the URL of a JPEG image of a celebrity.
 body = "{'url':'http://sheepmountain.com/wp-content/uploads/2016/01/20150131-DSC_0640-3-1500x700.jpg'}"
 
 try:
     # Execute the REST API call and get the response.
-
     conn = http.client.HTTPSConnection('westcentralus.api.cognitive.microsoft.com')
     conn.request("POST", "/vision/v1.0/analyze?%s" % params, body, headers)
     response = conn.getresponse()
