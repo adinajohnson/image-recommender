e00a552f573054f641261fbabb53dbaf4ac1f574
schumanns <lschuman@oberlin.edu>
2017-12-09 11:10:34 -0500

    Added textrank functionality

--- analyzeUrl.py
+++ analyzeUrl.py
@@ -3,6 +3,8 @@ from unsplash.api import Api
 from unsplash.auth import Auth
 from unsplash.photo import Photo
 import time
+from difflib import SequenceMatcher
+import keyworder
 
 
 def azureAnalysis(photo_url, subscription_key):
@@ -35,7 +37,7 @@ def azureAnalysis(photo_url, subscription_key):
 
         if 'statusCode' in data.keys():
             if data['statusCode'] == 429:
-                time.sleep(2)
+                time.sleep(10)
         else:
             tags = data['description']['tags']
             captions = data['description']['captions']
@@ -43,8 +45,9 @@ def azureAnalysis(photo_url, subscription_key):
 
     return tags, captions
 
+
 def unsplashRequest(tag, api, photo_worker):
-    photos = api.search.photos(tag)['results']
+    photos = api.search.photos(tag, per_page=15)['results']
 
     download_urls = []
     for photo in photos:
@@ -52,13 +55,14 @@ def unsplashRequest(tag, api, photo_worker):
 
     return download_urls
 
+
 def jaccard(A, B):
-    if type(A) == set and type(B) == set:
-        numer = len(A & B)
-        denom = len(A | B)
-        return numer/denom
-    else:
-        raise TypeError("Both A and B must be sets in order to calculate the Jaccard index.")
+    A = set(A)
+    B = set(B)
+
+    numer = len(A & B)
+    denom = len(A | B)
+    return numer/denom
 
 
 def write_picture_info(file, heading, url, tags, captions):
@@ -72,9 +76,9 @@ def write_picture_info(file, heading, url, tags, captions):
         file.write(str(captions).strip('[{}]'))
     file.write('\n\n')
 
-def main():
+def main(orig_photo_url, text):
     # get stuff so that we can write results to a file.
-    results_file = open('trial4.output', 'w')
+    results_file = open('trial5.output', 'w')
 
     #get unsplash API stuff
     client_id = os.environ.get('UNSPLASH_ID', None)
@@ -94,23 +98,22 @@ def main():
 
     azure_requests = 0
 
-    orig_photo_url = 'http://www.calgary.ca/CA/city-manager/scripts/about-us/webparts/images/ourHistory_retina.jpg'
-    orig_tags, orig_captions = azureAnalysis(orig_photo_url,
-                              azure_key)
+    orig_tags, orig_captions = azureAnalysis(orig_photo_url, azure_key)
 
     write_picture_info(results_file, 'Original Photo:', orig_photo_url, orig_tags, orig_captions)
 
+    search_terms = keyworder.get_search_terms(orig_tags, text)
 
     tagged_photos = {} # a dict which maps the url where we can find a photo to the tags of that photo
-    for tag in orig_tags:
-        download_urls = unsplashRequest(tag, api, photo_worker)
+    for term in search_terms:
+        download_urls = unsplashRequest(term, api, photo_worker)
         unsplash_requests += 11
         for i, url in enumerate(download_urls):
             try:
                 tags, captions = azureAnalysis(url, azure_key)
                 tagged_photos[url] = tags
 
-                heading = 'Search term: %s\tResult #%d' %(tag, i)
+                heading = 'Search term: %s\tResult #%d' %(term, i)
                 write_picture_info(results_file, heading, url, tags, captions)
             except KeyError:
                 print('KeyError with image. Image is probably "not accessible." Moving on to next one.')
@@ -118,11 +121,10 @@ def main():
         if unsplash_requests > 80 or azure_requests > 250:
             break
 
-    orig_tags = set(orig_tags)
     scores = {}
     for url, tags in tagged_photos.items():
-        tags = set(tags)
-        scores[url] = jaccard(orig_tags, tags)
+        sm = SequenceMatcher(orig_tags, tags)
+        scores[url] = sm.ratio()
 
     best_url = max(scores, key=scores.get)
     urllib.request.urlretrieve(best_url, 'BEST.jpeg')
@@ -134,4 +136,6 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    orig_photo_url = 'http://sheepmountain.com/wp-content/uploads/2016/01/20150131-DSC_0640-3-1500x700.jpg'
+    text = open('goat_mountain.output').read()
+    main(orig_photo_url, text)
---
+++ keyworder.py
@@ -0,0 +1,126 @@
+from math import log10
+import gensim
+from nltk.tokenize import casual_tokenize
+from collections import Counter
+from nltk.corpus import stopwords
+import nltk
+import string
+from statistics import mean
+
+
+class Vertex:
+
+    def __init__(self, word, count):
+        """
+        :param word: a word type from a given text
+        :param count: the count of that word in the text
+        """
+        self.word = word
+        self.count = count
+        self.score = log10(count*10)
+        self.sum_weights = None
+
+    def get_sum_weights(self, neighbors, weights):
+        self_weights = []
+        for vj in neighbors:
+            weight_self_j = weights[self.word][vj.word]
+            self_weights.append(weight_self_j)
+        self.sum_weights = sum(self_weights)
+
+    def update(self, d, neighbors, weights):
+        old_score = self.score
+
+        i_votes = []
+        for vj in neighbors:
+            weight_self_j = weights[self.word][vj.word]
+            weight_score = weight_self_j / vj.sum_weights
+            i_votes.append(weight_score * vj.score)
+
+        self.score = (1-d) + (d * sum(i_votes))
+
+        error = (self.score - old_score)**2
+        return error
+
+
+def get_search_terms(tags, text):
+    print('imports complete')
+
+    # get the model for the weights
+    model = gensim.models.KeyedVectors.load_word2vec_format('word2vec.6B.300d.txt')
+    print('model loaded')
+
+    tokens = list(casual_tokenize(text))
+    sw = set(stopwords.words())
+    modded_tokens = []
+
+    tags.reverse()
+    for i, pos in enumerate(tags):
+        for j in range(i + 1):
+            tokens.append(pos)
+
+    tokens = nltk.pos_tag(tokens)
+    for token, pos in tokens:
+        token = token.lower()
+        if token[-2:] == "'s":
+            token = token[:-2]
+        if token in string.punctuation or token.isdigit() or token in sw:
+            continue
+        elif token not in model.vocab:
+            continue
+
+        elif token in ['large', 'covered', 'background', 'standing', 'top', 'laying', 'tall', 'filled', 'many',
+                       'several']:
+            continue
+        elif token in tags:
+            modded_tokens.append(token)
+        elif pos not in ['NN', 'NNS', 'NNP', 'NPS', 'JJ']:
+            continue
+
+        else:
+            modded_tokens.append(token)
+    type_counts = Counter(modded_tokens)
+    types = list(type_counts)
+    print('word types retrieved. %d word types found' % len(types))
+
+    vertices = set()
+    weights = {word : {other_word : 0 for other_word in types} for word in types}
+    for word in types:
+        vertex = Vertex(word, type_counts[word])
+        vertices.add(vertex)
+        for other_word in types:
+            weights[word][other_word] = (model.similarity(word, other_word) + 1)/2
+    for v in vertices:
+        v.get_sum_weights(vertices, weights)
+    print('graph built')
+
+    print()
+    print('beginning convergence')
+    # now we begin the convergence
+    mean_square_error = 1
+    while mean_square_error > 0.00001:
+        sqer = []
+        for v in vertices:
+            error = v.update(0.85, vertices, weights)
+            sqer.append(error)
+        mean_square_error = mean(sqer)
+        print(mean_square_error)
+
+    search_terms = []
+    ordered = list(vertices)
+    ordered.sort(key=lambda x: getattr(x, 'score'), reverse=True)
+    for v in ordered:
+        if v.word in tags:
+            print('\t', v.word, v.count, v.score)
+            search_terms.append(v.word)
+        else:
+            print(v.word, v.count, v.score)
+
+    search_terms = search_terms[:3]
+    return search_terms
+
+
+if __name__ == "__main__":
+    tags = ['outdoor', 'mountain', 'nature', 'forest', 'background', 'snow', 'lake', 'standing', 'covered', 'front',
+            'tree', 'man', 'field', 'group', 'tall', 'hill', 'yellow', 'grazing', 'large', 'skiing', 'flock', 'slope']
+    text = open('goat_mountain.output').read()
+    get_search_terms(tags, text)
